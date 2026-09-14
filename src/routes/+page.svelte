840ea260702af723759d92af0f86d5097347b216
GayuhmA <gayuhmuktiaji9@gmail.com>
2026-09-14 18:36:36 +0700

    fix: typography

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="text-h1 text-primary-500 font-size-h1 font-h1">Test</div>
+<div class="text-body-3 font-outfit text-primary-500 bg-secondary-100 p-4 rounded-lg">Test Typography dan Warna</div>
